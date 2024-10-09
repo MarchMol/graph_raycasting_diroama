f2d44e2b6f47d8a2363ed594b8ea2f905ee8bc4a
MarchMol <marchmol98@gmail.com>
2024-10-08 21:24:43 -0600

    feat: paralelized render loop

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ use std::time::{Duration};
 use minifb::{Key, Window, WindowOptions};
 mod scene;
 mod texture;
+use rayon::prelude::*; 
+
 fn main() {
     let window_width = 600;
     let window_height = 600;
@@ -33,18 +35,18 @@ fn main() {
     .unwrap();
     let mut camera = Camera{
         eye: Vec3::new(0.0,0.0,-10.0), 
-        center:  Vec3::new(0.0,0.0,0.0), 
+        center:  Vec3::new(0.0,0.0,8.0), 
         up: Vec3::new(0.0,-1.0,0.0),
         has_changed: true};
 
 
         let l1 = Light::new(
-            Vec3::new(3.0, 2.0, 2.25),
-            Color::from_hex(0xd44a00),
-            0.5
+            Vec3::new(3.50, 2.0, 1.0),
+            Color::from_hex(0xff8000),
+            0.4
         );
         let l2 = Light::new(
-            Vec3::new(-10.0, 5.0, 0.0),
+            Vec3::new(-10.0, 5.0, 5.0),
             Color::new(255, 255, 255),
             0.6
         );
@@ -53,8 +55,13 @@ fn main() {
             Color::new(255, 255, 255),
             0.6
         );
+        let l4 = Light::new(
+            Vec3::new(2.5, 5.0, 7.5),
+            Color::from_hex(0xee00ff),
+            0.4
+        );
 
-    let lights = vec![l1, l2, l3];
+    let lights = vec![l1,l2, l3,l4];
     while window.is_open(){
         // closing game
         if window.is_key_down(Key::Escape) {
@@ -73,7 +80,7 @@ fn main() {
              camera.orbit(0.2, 0.0)
           }
           if window.is_key_down(Key::Q) {
-            camera.zoom(0.2);
+            camera.zoom(1.0);
         }
         if window.is_key_down(Key::E) {
             camera.zoom(-0.2);
@@ -103,31 +110,25 @@ pub fn render(framebuffer: &mut framebuffer::Framebuffer, objects: &[objects::Ob
     let aspect_ratio = width / height;
     
 
-    for y in 0..framebuffer.height {
-        for x in 0..framebuffer.width {
-            // Map the pixel coordinate to screen space [-1, 1]
-            let screen_x = (2.0 * x as f32) / width - 1.0;
-            let screen_y = -(2.0 * y as f32) / height + 1.0;
+    framebuffer.buffer.par_chunks_mut(framebuffer.width as usize)
+        .enumerate()
+        .for_each(|(y, row)| {
+            let screen_y = (2.0 * y as f32) / height -1.0;
+            for (x, pixel) in row.iter_mut().enumerate() {
+                let screen_x = (2.0 * x as f32) / width - 1.0;
+                let screen_x = screen_x * aspect_ratio;
 
-            // Adjust for aspect ratio
-            let screen_x = screen_x * aspect_ratio;
+                let ray_direction = normalize(
+                    &camera.basis_change(&Vec3::new(screen_x, screen_y, -1.0))
+                );
 
-            // Calculate the direction of the ray for this pixel
-            let ray_direction = normalize(
-                &camera.basis_change(&Vec3::new(screen_x, screen_y, -1.0))
-            );
+                let pixel_color: u32 = ray_caster::cast_ray(
+                    &camera.eye, &ray_direction,
+                    objects, lights,
+                    0
+                );
 
-
-            // Cast the ray and get the pixel color
-            let pixel_color:u32 = ray_caster::cast_ray(
-                
-                &camera.eye, &ray_direction,
-                 objects,lights,
-                0);
-
-            // Draw the pixel on screen with the returned color
-            framebuffer.set_current_color(pixel_color);
-            framebuffer.point(x, y);
-        }
-    }
+                *pixel = Color::from_hex(pixel_color);
+            }
+        });
 }
